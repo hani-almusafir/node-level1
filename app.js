13ad727f8cb815e5a94f62847f8f4b00ce6853d9
hani-almusafir <almusafir105@gmail.com>
2025-04-14 13:24:15 +0300

    connect-db

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
+const mongoose = require("mongoose");
 
-app.get('/', (req, res) => {
-//   res.send('Hello World!')
-  res.sendFile("./views/home.html", { root: __dirname })    
-})
+app.get("/", (req, res) => {
+  //   res.send('Hello World!')
+  res.sendFile("./views/home.html", { root: __dirname });
+});
 
 app.listen(port, () => {
-  console.log(`http://localhost:${port}/`)
-})  
+  console.log(`http://localhost:${port}/`);
+});
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/test")
+  .then(() => {})
+  .catch((err) => {});
